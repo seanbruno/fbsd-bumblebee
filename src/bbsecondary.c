b3f666372ec7472f45b6a3c414243b908ec974c1
Sean Bruno <sbruno@freebsd.org>
2012-08-27 18:52:09 -0700

    More freebsd specific things to make autoconf/make happy. also, what the hell linux?  pipe2?  REALLY?

--- src/bbsecondary.c
+++ src/bbsecondary.c
@@ -162,7 +162,11 @@ void start_secondary(void) {
     if (bb_status.x_pipe[0] != -1){close(bb_status.x_pipe[0]); bb_status.x_pipe[0] = -1;}
     if (bb_status.x_pipe[1] != -1){close(bb_status.x_pipe[1]); bb_status.x_pipe[1] = -1;}
     //create a new pipe
+#ifdef __FreeBSD__
+    if (pipe(bb_status.x_pipe)){
+#else
     if (pipe2(bb_status.x_pipe, O_NONBLOCK)){
+#endif
       set_bb_error("Could not create output pipe for X");
       return;
     }
